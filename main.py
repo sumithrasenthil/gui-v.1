176c93c21f29cb9e6ec20fcea73de638e63bc2b1
Sumithra <54926196+sumithrasenthil@users.noreply.github.com>
2019-12-19 16:02:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import time
 
 from PyQt5 import QtCore, QtGui, QtWidgets, QtMultimedia, QtMultimediaWidgets
+from PyQt5.QtGui import QMovie, QPainter, QPixmap
 from PyQt5.QtCore import QRect, pyqtSlot, QSize
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QLineEdit, QMessageBox, QSizePolicy, QStyle, QLabel, QDialog
@@ -10,6 +11,11 @@ from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QLineEdit, QMessageBox, QS
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import (QApplication, QDialog,
                              QProgressBar, QPushButton)
+from   PyQt5.QtWidgets import *
+from   PyQt5.QtGui     import *
+from   PyQt5.QtCore    import *
+
+GIF = 'charging1.gif'
 
 from pprogress import External
 
@@ -51,56 +57,71 @@ class VideoWindow(QtWidgets.QMainWindow):
 
         #Heading of the window .
         self.label = QtWidgets.QLabel(self)
-        self.label.setText("<h1>CHARGEPAY WELCOMES YOU</h2>")
-        self.label.setGeometry(QRect(350, 10, 525, 145))
-        self.label.setStyleSheet('color:darkblack')
+        self.label.setText("<h1>ChargePay Welcomes You</h1>")
+        self.label.setFont(QtGui.QFont("Bitter",20))
+        self.label.setGeometry(QRect(150, 40, 925, 145))
+        self.label.setStyleSheet('color:#1B9CFC')
 
         # welcome user heading
         
         self.label = QtWidgets.QLabel(self)
-        self.label.setText("<h2>Welcome User</h3>")
-        self.label.setGeometry(QRect(950, 110, 525, 145))
-        self.label.setStyleSheet('color:darkred')
-        self.label.setFont(QtGui.QFont("Bitter"))
-        self.label2 = QLabel(self)
-        self.label2.setPixmap(QtGui.QPixmap('user2.png'))
-        self.label2.setGeometry(900,159, 40, 40)
-        self.label3 = QLabel(self)
+        self.label.setText("<h1>Welcome User</h1>")
+        self.label.setFont(QtGui.QFont("Bitter",15))
+
+        self.label.setGeometry(QRect(900, 170, 625, 145))
+        self.label.setStyleSheet('color:#1B9CFC')
+#         self.label.setFont(QtGui.QFont("Bitter"))
+#         self.label2 = QLabel(self)
+#         self.label2.setPixmap(QtGui.QPixmap('user2.png'))
+#         self.label2.setGeometry(900,209, 30, 30)
+#         self.label3 = QLabel(self)
 
 
 
         # state of charge
-        button1 = QPushButton(" SOC ", self)
-        button1.setStyleSheet('QPushButton {background-color: black ,color: darkred;}')
+        self.button1 = QtWidgets.QLabel(self)
+        self.button1.setText("<h1>Soc</3>")
+        self.button1.setFont(QtGui.QFont("Bitter",13))
 
-        button1.setGeometry(QRect(940, 270, 100, 45))
-        button1.resize(50, 35)
-        button1.setToolTip("STATE OF CHARGE")
+        #self.button1.setGeometry(QRect(900, 170, 625, 145))
+        #self.button1.setStyleSheet('color:darkred')
+        #button1 = QPushButton(" SOC ", self)
+        #button1.setStyleSheet('QPushButton {background-color: black ,color: darkred;}')
+        #button1.setStyleSheet('color:darkblack')
+
+        self.button1.setGeometry(QRect(940, 300, 100, 45))
+#         self.button1.resize(70, 55)
+        self.button1.setToolTip("STATE OF CHARGE")
 
         # soc output
         button1 = QPushButton("  ", self)
-        button1.setGeometry(QRect(1070, 270, 100, 45))
-        button1.resize(60, 35)
+        button1.setGeometry(QRect(1070, 300, 80, 55))
+        button1.resize(100, 45)
         button1.setToolTip("STATE OF CHARGE")
 
         # required units
-        button1 = QPushButton(" UNITS ", self)
+        self.button1 = QtWidgets.QLabel(self)
+        self.button1.setText("<h1>Units</3>")
+        self.button1.setFont(QtGui.QFont("Bitter",13))
 
-        button1.setGeometry(QRect(940, 370, 100, 45))
-        button1.resize(50, 35)
-        button1.setToolTip("Units needed for the user ")
+        self.button1.setGeometry(QRect(940, 370, 100, 45))
+        #button1.resize(50, 35)
+        #button1.setToolTip("Units needed for the user ")
 
         # input fields for charging
         self.textbox = QLineEdit(self)
-        self.textbox.setGeometry(QRect(1070, 370, 100, 45))
-        self.textbox.resize(60, 35)
+        self.textbox.setGeometry(QRect(1070, 370, 700, 145))
+        self.textbox.resize(100, 45)
 
         # Create a button in the window
-        self.button2 = QPushButton("Charge  ", self)
-        self.button2.setGeometry(QRect(980, 470, 100, 45))
+        self.button2 = QPushButton("Charge", self)
+        self.button2.setGeometry(QRect(980, 500, 180, 60))
         self.button2.setStyleSheet('color:darkred')
+        #self.button2.setStyleSheet('QPushButton{background-color:black;color:red;}')
+        self.button2.setFont(QtGui.QFont("Bitter",23))
+
         self.button2.setIcon(QtGui.QIcon("icon.png"))
-        self.button2.setIconSize(QtCore.QSize(30, 30))
+        self.button2.setIconSize(QtCore.QSize(40, 40))
         self.button2.setToolTip("Proceed to Charge")
         self.lineEdit1 = QLineEdit("Type here what you want to transfer for [Window1].", self)
 
@@ -111,43 +132,68 @@ class VideoWindow(QtWidgets.QMainWindow):
 
         #contact
         self.label1 = QtWidgets.QLabel(self)
-        self.label1.setText("<h3>Contact</h2>")
+        self.label1.setText("<h1>Contact</h1>")
         self.label1.setGeometry(QRect(940, 640, 400, 45))
-        self.label1.setStyleSheet('color:darkred')
-        self.label2 = QLabel(self)
-        self.label2.setPixmap(QtGui.QPixmap('contact1.png'))
-        self.label2.setGeometry(910, 640, 40, 40)
+        self.label1.setStyleSheet('color:#1B9CFC')
+        self.label1.setFont(QtGui.QFont("Bitter",15))
+
+        #self.label2 = QLabel(self)
+        #self.label2.setPixmap(QtGui.QPixmap('contact1.png'))
+        #self.label2.setGeometry(910, 640, 40, 40)
         # self.label3 = QLabel(self)
         # self.label3.setPixmap(QtGui.QPixmap('line.png'))
         # self.label3.setGeometry(540, 375, 200, 10)
         self.label1 = QtWidgets.QLabel(self)
-        self.label1.setText("<h4>xyz@gmail.com</h4>")
-        self.label1.setGeometry(QRect(985, 700, 400, 45))
+        self.label1.setText("<h2>xyz@gmail.com</h2>")
+        self.label1.setGeometry(QRect(985, 710, 400, 45))
         self.label2 = QLabel(self) # self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.label2.setPixmap(QtGui.QPixmap('gmail.png'))
-        self.label2.setGeometry(955, 705, 35, 35)
-        self.label3 = QLabel(self)
+        #self.label2.setPixmap(QtGui.QPixmap('gmail.png'))
+        #self.label2.setGeometry(955, 705, 35, 35)
+        #self.label3 = QLabel(self)
 
         self.label1 = QtWidgets.QLabel(self)
-        self.label1.setText("<h4>1223-3542-2334</h4>")
-        self.label1.setGeometry(QRect(985, 735, 400, 45))
+        self.label1.setText("<h2>1223-3542-2334</h2>")
+        self.label1.setGeometry(QRect(985, 765, 400, 45))
         self.label2 = QLabel(self)
-        self.label2.setPixmap(QtGui.QPixmap('phone2.png'))
-        self.label2.setGeometry(960,740, 40, 40)
-        self.label3 = QLabel(self)
+        #self.label2.setPixmap(QtGui.QPixmap('phone2.png'))
+        #self.label2.setGeometry(960,740, 40, 40)
+        #self.label3 = QLabel(self)
 
 
 
 
         # visit again..
+        
         self.label1 = QtWidgets.QLabel(self)
-        self.label1.setText("<h3> VISIT AGAIN !!!</h2>")
-        self.label1.setGeometry(QRect(970, 810, 400, 45))
-        self.label1.setStyleSheet('color:darkred')
-        self.label2 = QLabel(self)
-        self.label2.setPixmap(QtGui.QPixmap('heart5.png'))
-        self.label2.setGeometry(930, 810, 40, 40)
+        self.label1.setText("<h1>Reduce Your Footprint</h1>")
+        self.label1.setFont(QtGui.QFont("Bitter",17))
+        self.label1.setGeometry(QRect(100, 735, 925, 145))
+        self.label1.setStyleSheet('color:#4caf50')
+        self.label2 = QtWidgets.QLabel(self)
+        self.label2.setText("<h1>And</h1>")
+        self.label2.setFont(QtGui.QFont("Bitter",17))
+        self.label2.setGeometry(QRect(390, 800, 925, 145))
+        self.label2.setStyleSheet('color:#4caf50')
+        self.label3 = QtWidgets.QLabel(self)
+        self.label3.setText("<h1>Go  Green !!!</h1>")
+        self.label3.setFont(QtGui.QFont("Bitter",17))
+        self.label3.setGeometry(QRect(280, 865, 925, 145))
+        self.label3.setStyleSheet('color:#4caf50')
+
+        
+        
+        
+        
+        #self.label1 = QtWidgets.QLabel(self)
+        #self.label1.setText("<h1> VISIT AGAIN !!!</h1>")
+        #self.label1.setGeometry(QRect(470, 880, 400, 45))
+        #self.label1.setStyleSheet('color:darkred')
+        #self.label2 = QLabel(self)
+        #self.label2.setPixmap(QtGui.QPixmap('redheart2.png'))
+        #self.label2.setGeometry(560, 900, 64, 64)
+        #self.label2.setFont(QtGui.QFont("Bitter",20))
+
 
         # self.label = QLabel(self)
         # self.label.setPixmap(QPixmap('payment5.jpg'))
@@ -169,7 +215,7 @@ class VideoWindow(QtWidgets.QMainWindow):
 
         container = QtWidgets.QWidget()
         lay = QtWidgets.QVBoxLayout(container)
-        lay.setContentsMargins(50, 100, 550, 10)
+        lay.setContentsMargins(50, 130, 550, 10)
 
         lay.addWidget(videoWidget)
 
@@ -214,7 +260,7 @@ class VideoWindow(QtWidgets.QMainWindow):
 
         # Create layouts to place inside widget
         controlLayout = QtWidgets.QHBoxLayout()
-        controlLayout.setContentsMargins(100, 200, 550, 200)
+        controlLayout.setContentsMargins(0, 0, 550, 200)
         controlLayout.addWidget(self.playButton)
         controlLayout.addWidget(self.positionSlider)
 
@@ -239,12 +285,14 @@ class VideoWindow(QtWidgets.QMainWindow):
         print(textboxValue)
 
         self.textbox.setText("")
+        
     @pyqtSlot()
     def button2_onClick(self):
         self.statusBar().showMessage("Switched to window 1")
         self.cams = Window1(self.lineEdit1.text())
         self.cams.show()
         self.close()
+    
 
     # video player code
     def openFile(self):
@@ -289,56 +337,84 @@ class Window1(QDialog):
 
         self.top = 0
         self.left = 100
-        self.width = 720
-        self.height = 480
+        self.width = 1400
+        self.height = 1000
         self.InitUI()
         # player.resize(2100, 3470)
 
     def InitUI(self):
+        
+       
+        #self.movie = QMovie("charging1.gif")
+        #self.movie.frameChanged.connect(self.repaint)
+        #self.movie.setGeometry(100, 100, 800, 800)
+        
+        #self.movie.start()
 
         self.label = QLabel(self)
-        self.label.setPixmap(QPixmap('payment5.jpg'))
-        self.label.setGeometry(0, 0, 3800, 400)
-        # self.show()
+        self.label.setPixmap(QPixmap('testing3.jpeg'))
+        self.label.setGeometry(100, 150, 600, 600)
+        #self.show()
 
         self.setGeometry(self.top, self.left, self.width, self.height)
         self.setWindowTitle('Amount')
         self.setWindowIcon(self.style().standardIcon(QStyle.SP_FileDialogInfoView))
-
+         
+        
+         
 
         # second window Header
         self.label = QtWidgets.QLabel(self)
-        self.label.setText("<h2>ChargePay Welcomes you</h2>")
-        self.label.setGeometry(QRect(275, -30, 525, 145))
-        self.label.setStyleSheet('color:darkred')
+        self.label.setText("<h1>ChargePay Welcomes you</h1>")
+        self.label.setGeometry(QRect(150, 20, 925, 145))
+        self.label.setFont(QtGui.QFont("Bitter",20))
+        self.label.setStyleSheet('color:darkblue')
+        
 
-        # amount Input
-        button1 = QPushButton(" Amount ", self)
-        button1.setGeometry(QRect(1510, 100, 100, 45))
-        button1.resize(60, 35)
-        button1.setToolTip("Amount")
+        self.label1 = QtWidgets.QLabel(self)
+        self.label1.setText("<h1>Hello User</h1>")
+        #self.label.setGeometry(QRect(900, 170, 625, 145))
+        self.label1.setGeometry(QRect(930, 150, 525, 145))
+        self.label1.setStyleSheet('color:darkblue')
+        self.label1.setFont(QtGui.QFont("Bitter",18))
+        
+        
+        
+        button1 = QtWidgets.QLabel(self)
+        button1.setText("<h1>Amount</h1>")
+        button1.setFont(QtGui.QFont("Bitter",13))
+        button1.setStyleSheet('color:darkblack')
+
+        button1.setGeometry(QRect(910, 320, 150, 45))
+        
+        
 
         #  Amount output
         button1 = QPushButton("  ", self)
-        button1.setGeometry(QRect(1650, 100, 100, 45))
-        button1.resize(60, 35)
+        button1.setGeometry(QRect(1100, 320, 100, 45))
+        button1.resize(100, 45)
         button1.setToolTip("Amount for the respective units")
 
         # Progress bar header
         self.label1 = QtWidgets.QLabel(self)
-        self.label1.setText("<h3>Charging</h2>")
-        self.label1.setGeometry(QRect(1555,165,100,45))
-        self.label1.setStyleSheet('color:darkred')
+        self.label1.setText("<h1>Charging</h1>")
+        self.label1.setFont(QtGui.QFont("Bitter",13))
+        self.label1.setGeometry(QRect(955,415,190,45))
+        self.label1.setStyleSheet('color:darkblue')
 
         #Progress bar for charging
         self.progress = QProgressBar(self)
-        self.progress.setGeometry(1510, 225, 200, 25)
+        self.progress.setGeometry(995, 525, 200, 25)
         self.progress.setMaximum(100)
         self.button = QPushButton('start', self)
-        self.button.move(1555, 270)
-
+        self.button.setGeometry(1000, 575, 100, 30)
         self.button.clicked.connect(self.onButtonClick)
-
+        
+     
+        
+        
+        
+#         
 
         #Bach button for first window
         layoutV = QVBoxLayout()
@@ -350,19 +426,24 @@ class Window1(QDialog):
 
 
         # thanksGiving for user
-        self.label1 = QtWidgets.QLabel(self)
-        self.label1.setText("<h3>THANKS FOR COMING !!!</h2>")
-        self.label1.setGeometry(QRect(1500, 350, 400, 45))
-        self.label1.setStyleSheet('color:darkred')
+           
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("<h1>Thanks for Charging</h1>")
+        self.label.setFont(QtGui.QFont("Bitter",20))
+        self.label.setGeometry(QRect(300, 775, 925, 145))
+        self.label.setStyleSheet('color:darkred')
 
-        self.label1 = QtWidgets.QLabel(self)
-        self.label1.setText("<h3>HAPPY JOURNEY</h2>")
-        self.label1.setGeometry(QRect(1530, 380, 300, 45))
-        self.label1.setStyleSheet('color:darkred')
-        self.label2 = QLabel(self)
-        self.label2.setPixmap(QtGui.QPixmap('heart5.png'))
-        self.label2.setGeometry(1690, 380, 40, 40)
-
+        
+        
+        
+        
+        #self.label1 = QtWidgets.QLabel(self)
+        #self.label1.setText("<h1> VISIT AGAIN !!!</h1>")
+        #self.label1.setGeometry(QRect(470, 880, 400, 45))
+        #self.label1.setStyleSheet('color:darkred')
+        #self.label2 = QLabel(self)
+        #self.label2.setPixmap(QtGui.QPixmap('redheart2.png'))
+        #self.label2.setGeometry(560, 900, 64, 64)
         # icon
         # self.label3= QtWidgets.QLabel(self)
         # self.label3.setText("<h3>HAPPY JOURNEY</h2>")
@@ -370,12 +451,16 @@ class Window1(QDialog):
         # self.label3.setIcon(QtGui.QIcon("heart1.png"))
         # self.label3.setIconSize(QtCore.QSize(30, 30))
         # self.label3.setToolTip("Proceed to Charge")
-        # # self.lineEdit1 = QLineEdit("Type here what you want to transfer for [Window1].", self)
-
+        #self.lineEdit1 = QLineEdit("Type here what you want to transfer for [Window1].", selfdef onButtonClick(self):
+        self.calc = External()
+        self.calc.countChanged.connect(self.onCountChanged)
+        self.calc.start()
+        
     def onButtonClick(self):
         self.calc = External()
         self.calc.countChanged.connect(self.onCountChanged)
         self.calc.start()
+    
 
     def onCountChanged(self, value):
         self.progress.setValue(value)
@@ -396,6 +481,7 @@ class External(QThread):
             count += 10
             time.sleep(1)
             self.countChanged.emit(count)
+
 
 
 
